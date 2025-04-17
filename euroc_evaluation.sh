2136197624d430ee2514bd9368f396620f974dc6
Thanh Nguyen Canh <canhthanhlt@gmail.com>
2025-04-17 17:36:14 +0900

    update

--- euroc_evaluation.sh
+++ euroc_evaluation.sh
@@ -5,4 +5,4 @@
 echo "------------------------------------"
 echo "Evaluation of MH01 trajectory with Monocular sensor"
 
-python3 evaluation/evaluate_ate_scale_py3.py evaluation/Ground_truth/EuRoC_left_cam/MH01_GT.txt results/f_dataset-MH01-mono.txt --plot MH01_mono.pdf
+python3 evaluation/py3_evaluate_ate_scale.py evaluation/Ground_truth/EuRoC_left_cam/MH01_GT.txt results/f_dataset-MH01-mono.txt --plot MH01_mono.pdf
