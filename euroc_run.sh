71e80c993e8375b716907b2a7f088a44b3658587
Thanh Nguyen Canh <canhthanhlt@gmail.com>
2025-04-18 16:25:11 +0900

    update

--- euroc_run.sh
+++ euroc_run.sh
@@ -3,4 +3,4 @@
 #------------------------------------
 # Monocular Examples
 echo "Launching MH01 with Monocular sensor"
-./Test/Monocular/mono_euroc ./Vocabulary/ORBvoc.txt ./Test/Monocular/EuRoC.yaml ../euroc_dataset/machine_hall/MH_01_easy ./Test/Monocular/EuRoC_TimeStamps/MH01.txt dataset-MH01-mono
+./Test/Monocular/mono_euroc ./Vocabulary/ORBvoc.txt ./Test/Monocular/EuRoC.yaml ../euroc_dataset/machine_hall/MH_01_easy ./Test/Monocular/EuRoC_TimeStamps/MH01.txt iraf/euroc/MH01
